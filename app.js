89f9aed61e58f9e87b31f16099cf178ce6bb8139
anuj <anujtech46>
2017-05-31 18:51:16 +0530

    Issue #1 fix: minor issue

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(methodOverride());
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,cid, *');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,cid,user-id *');
 
     if ('OPTIONS' === req.method) {
         res.sendStatus(200);
@@ -46,11 +46,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-require('./middlewares/proxy.middleware')(app);
 require('./routes/courseRoutes')(app);
 require('./routes/contentRoutes')(app);
 require('./routes/utilsRoute')(app);
 require('./routes/notesRoutes')(app);
+//last this middle in last
+require('./middlewares/proxy.middleware')(app);
 
 
 
