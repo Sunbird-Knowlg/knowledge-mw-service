5379aa34a3786aa03f8f2da136789c33e6b9794b
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-05 20:40:47 +0530

    Issue #1 feat : Update API paths

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -8,8 +8,12 @@ var contentService = require('../service/contentService');
 var requestMiddleware = require('../middlewares/request.middleware');
 
 var BASE_URL_V1 = "/api/sb/v1/content";
+var BASE_URL = "/v1/content";
 
 module.exports = function(app) {
+
+    app.route('/health')
+        .get(contentService.checkHealth);
 
     app.route(BASE_URL_V1 + '/search')
         .post(requestMiddleware.createAndValidateRequestBody, contentService.searchContentAPI);
@@ -34,4 +38,31 @@ module.exports = function(app) {
 
     app.route(BASE_URL_V1 + '/get/mycontent/:createdBy')
         .get(requestMiddleware.createAndValidateRequestBody, contentService.getMyContentAPI);
+
+    app.route(BASE_URL + '/search')
+        .post(requestMiddleware.createAndValidateRequestBody, contentService.searchContentAPI);
+
+    app.route(BASE_URL + '/create')
+        .post(requestMiddleware.createAndValidateRequestBody, contentService.createContentAPI);
+
+    app.route(BASE_URL + '/update/:contentId')
+        .patch(requestMiddleware.createAndValidateRequestBody, contentService.updateContentAPI);
+
+    app.route(BASE_URL + '/upload/:contentId')
+        .post(requestMiddleware.createAndValidateRequestBody, contentService.uploadContentAPI);
+
+    app.route(BASE_URL + '/review/:contentId')
+        .post(requestMiddleware.createAndValidateRequestBody, contentService.reviewContentAPI);
+
+    app.route(BASE_URL + '/publish/:contentId')
+        .get(requestMiddleware.createAndValidateRequestBody, contentService.publishContentAPI);
+
+    app.route(BASE_URL + '/read/:contentId')
+        .get(requestMiddleware.createAndValidateRequestBody, contentService.getContentAPI);
+
+    app.route(BASE_URL + '/read/mycontent/:createdBy')
+        .get(requestMiddleware.createAndValidateRequestBody, contentService.getMyContentAPI);
+
+    app.route(BASE_URL + '/retire')
+        .get(contentService.checkHealth);
 };
