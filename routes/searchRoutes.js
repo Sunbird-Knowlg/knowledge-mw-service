051a2ba2dee2706e77a780d07d3f8e7f0b405886
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-05 13:11:57 +0530

    add composite search API

---
+++ routes/searchRoutes.js
@@ -0,0 +1,17 @@
+/**
+ * file: search-route.js
+ * author: Anuj Gupta
+ * desc: route file for content
+ */
+
+var contentService = require('../service/contentService');
+var requestMiddleware = require('../middlewares/request.middleware');
+
+var BASE_URL_V1 = "/v1";
+
+module.exports = function(app) {
+
+    app.route(BASE_URL_V1 + '/search')
+        .post(requestMiddleware.createAndValidateRequestBody, contentService.searchContentAPI);
+    
+};
