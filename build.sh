ea08dee1959affdecdd0ac9a60728ed9667214b4
manoj v <manojv@ilimi.in>
2017-07-13 11:40:58 +0530

    Issue #6 feat : Changed the paths in the docker file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -f ./content/services/js-services/content_service/Dockerfile.Build -t sunbird/content_service:0.0.1-build . 
 docker run --name content_service-0.0.1-build sunbird/content_service:0.0.1-build 
 containerid=`docker ps -q -a -f name=content_service-0.0.1-build`
-docker cp $containerid:/opt/content_service.zip content/services/js-services/content_service/content_service.zip 
+docker cp $containerid:/opt/content_service.zip ./content/services/js-services/content_service/
 docker rm $containerid
 docker build -f ./content/services/js-services/content_service/Dockerfile -t sunbird/content_service:0.0.1-bronze .
