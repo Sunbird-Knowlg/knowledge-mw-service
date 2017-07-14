ffafcf6731791b24c18340c320251d49e470fb8e
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-14 17:38:21 +0530

    Issue #000 feat : proxy API

--- middlewares/proxy.middleware.js
+++ middlewares/proxy.middleware.js
@@ -33,4 +33,14 @@ module.exports = function (app) {
             return require('url').parse(ceUrl + req.originalUrl).path;
         }
     }));
+
+    app.use('/v1/telemetry', proxy(ekstep, {
+        proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+            proxyReqOpts.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkNjNiMjgwZTQ1NDE0NDU4ODk4NzcwYzZhOGZiZjQ1MCJ9.Ji-22XcRrOiVy4dFAmE68wPxLkNmX4wKbTj_IB7fG6Y';
+            return proxyReqOpts;
+        },
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep + '/api/data/v3/telemetry').path;
+        }
+    }));
 };
