1bd5e985d19ec475f988f53592496c77446b5691
manoj v <manojv@ilimi.in>
2017-07-14 10:23:54 +0530

    Issue #6 feat : changed the tag name for the build image

--- dockerPushToRepo.sh
+++ dockerPushToRepo.sh
@@ -1,4 +1,4 @@
 artifactLabel=${ARTIFACT_LABEL:-bronze}
 docker login -u "purplesunbird" -p`cat /run/secrets/hub-pass`
-docker push content_service:0.0.1-bronze
+docker push sunbird/content_service:0.0.1-bronze
 docker logout
