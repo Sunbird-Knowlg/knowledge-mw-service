b0816a3aaa96673310c104333ae9ecdce74200d2
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-21 02:26:27 +0530

    Issue #000 feat : change proxy settings

--- middlewares/proxy.middleware.js
+++ middlewares/proxy.middleware.js
@@ -3,44 +3,57 @@ var proxy = require('express-http-proxy');
 
 module.exports = function (app) {
 
-    var ekstep = "https://qa.ekstep.in";
-    var ceUrl = 'https://s3.ap-south-1.amazonaws.com/ekstep-public-qa';
+    var ekstep_api = global_ekstep_api_base_url;
+    var ekstep_proxy = global_ekstep_proxy_base_url
+    var api_key = global_ekstep_api_key;
 
-    app.use('/api/*', proxy(ekstep, {
-
-        proxyReqPathResolver: function (req) {
-            return require('url').parse(ekstep + req.originalUrl).path;
-        }
-    }));
-
-    app.use('/content-plugins/*', proxy(ceUrl, {
-
-        proxyReqPathResolver: function (req) {
-            return require('url').parse(ceUrl + req.originalUrl).path;
-        }
-    }));
-
-    app.use('/plugins/*', proxy(ceUrl, {
-
-        proxyReqPathResolver: function (req) {
-            return require('url').parse(ceUrl + req.originalUrl).path;
-        }
-    }));
-
-    app.use('/assets/public/preview/*', proxy(ceUrl, {
-
-        proxyReqPathResolver: function (req) {
-            return require('url').parse(ceUrl + req.originalUrl).path;
-        }
-    }));
-
-    app.use('/v1/telemetry', proxy(ekstep, {
+    app.use('/api/*', proxy(ekstep_api, {
         proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-            proxyReqOpts.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkNjNiMjgwZTQ1NDE0NDU4ODk4NzcwYzZhOGZiZjQ1MCJ9.Ji-22XcRrOiVy4dFAmE68wPxLkNmX4wKbTj_IB7fG6Y';
+            proxyReqOpts.headers['Authorization'] = 'Bearer ' + api_key;
             return proxyReqOpts;
         },
         proxyReqPathResolver: function (req) {
-            return require('url').parse(ekstep + '/api/data/v3/telemetry').path;
+            return require('url').parse(ekstep_api + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/content-plugins/*', proxy(ekstep_proxy, {
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_proxy + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/plugins/*', proxy(ekstep_proxy, {
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_proxy + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/assets/public/preview/*', proxy(ekstep_proxy, {
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_proxy + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/content/preview/*', proxy(ekstep_proxy, {
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_proxy + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/action/*', proxy(ekstep_proxy, {
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_proxy + req.originalUrl).path;
+        }
+    }));
+
+    app.use('/v1/telemetry', proxy(ekstep_api, {
+        proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+            proxyReqOpts.headers['Authorization'] = 'Bearer ' + api_key;
+            return proxyReqOpts;
+        },
+        proxyReqPathResolver: function (req) {
+            return require('url').parse(ekstep_api + '/api/data/v3/telemetry').path;
         }
     }));
 };
--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -7,42 +7,11 @@
 var contentService = require('../service/contentService');
 var requestMiddleware = require('../middlewares/request.middleware');
 
-var BASE_URL_V1 = "/api/sb/v1/content";
 var BASE_URL = "/v1/content";
 
 module.exports = function(app) {
 
     app.route('/health').get(contentService.checkHealth);
-
-    app.route(BASE_URL_V1 + '/search')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.searchContentAPI);
-
-    app.route(BASE_URL_V1 + '/create')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.createContentAPI);
-
-    app.route(BASE_URL_V1 + '/update/:contentId')
-        .patch(requestMiddleware.createAndValidateRequestBody, contentService.updateContentAPI);
-
-    app.route(BASE_URL_V1 + '/upload/:contentId')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.uploadContentAPI);
-
-    app.route(BASE_URL_V1 + '/review/:contentId')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.reviewContentAPI);
-
-    app.route(BASE_URL_V1 + '/publish/:contentId')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.publishContentAPI);
-
-    app.route(BASE_URL_V1 + '/get/:contentId')
-        .get(requestMiddleware.createAndValidateRequestBody, contentService.getContentAPI);
-
-    app.route(BASE_URL_V1 + '/get/mycontent/:createdBy')
-        .get(requestMiddleware.createAndValidateRequestBody, contentService.getMyContentAPI);
-
-    app.route(BASE_URL_V1 + '/retire/:contentId')
-        .delete(requestMiddleware.createAndValidateRequestBody, contentService.retireContentAPI);
-
-    app.route(BASE_URL_V1 + '/reject/:contentId')
-        .post(requestMiddleware.createAndValidateRequestBody, contentService.rejectContentAPI);
 
     app.route(BASE_URL + '/search')
         .post(requestMiddleware.createAndValidateRequestBody, contentService.searchContentAPI);
