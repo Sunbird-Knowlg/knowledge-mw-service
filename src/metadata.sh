05bf3c12b361d01e9889cb8e89a345dfff3b5f31
manoj v <manojv@ilimi.in>
2017-09-11 10:49:24 +0530

    Issue #1 fix : metadata.sh version increased

--- src/metadata.sh
+++ src/metadata.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # return version
-echo '{"name":"content_service","version":"0.0.1","org":"sunbird","hubuser":"purplesunbird"}'
+echo '{"name":"content_service","version":"0.0.2","org":"sunbird","hubuser":"purplesunbird"}'
