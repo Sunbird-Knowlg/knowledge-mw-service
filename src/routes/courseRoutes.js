952ec7492e19b738b482a3b8929349f9db90fbb2
Harishkumar Gangula <harishg@ilimi.in>
2018-01-16 10:59:57 +0530

    Issue #SB-1400 fix: standard fixes for route js files

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -4,38 +4,37 @@
  * desc: route file for course
  */
 
-var courseService = require('../service/courseService');
-var contentService = require('../service/contentService');
-var requestMiddleware = require('../middlewares/request.middleware');
+var courseService = require('../service/courseService')
+var contentService = require('../service/contentService')
+var requestMiddleware = require('../middlewares/request.middleware')
 
-var BASE_URL = "/v1/course";
+var BASE_URL = '/v1/course'
 
-module.exports = function(app) {
+module.exports = function (app) {
+  app.route(BASE_URL + '/search')
+    .post(requestMiddleware.createAndValidateRequestBody, courseService.searchCourseAPI)
 
-    app.route(BASE_URL + '/search')
-        .post(requestMiddleware.createAndValidateRequestBody, courseService.searchCourseAPI);
+  app.route(BASE_URL + '/create')
+    .post(requestMiddleware.createAndValidateRequestBody, courseService.createCourseAPI)
 
-    app.route(BASE_URL + '/create')
-        .post(requestMiddleware.createAndValidateRequestBody, courseService.createCourseAPI);
+  app.route(BASE_URL + '/update/:courseId')
+    .patch(requestMiddleware.createAndValidateRequestBody, courseService.updateCourseAPI)
 
-    app.route(BASE_URL + '/update/:courseId')
-        .patch(requestMiddleware.createAndValidateRequestBody, courseService.updateCourseAPI);
+  app.route(BASE_URL + '/review/:courseId')
+    .post(requestMiddleware.createAndValidateRequestBody, courseService.reviewCourseAPI)
 
-    app.route(BASE_URL + '/review/:courseId')
-        .post(requestMiddleware.createAndValidateRequestBody, courseService.reviewCourseAPI);
+  app.route(BASE_URL + '/publish/:courseId')
+    .get(requestMiddleware.createAndValidateRequestBody, courseService.publishCourseAPI)
 
-    app.route(BASE_URL + '/publish/:courseId')
-        .get(requestMiddleware.createAndValidateRequestBody, courseService.publishCourseAPI);
+  app.route(BASE_URL + '/read/:courseId')
+    .get(requestMiddleware.createAndValidateRequestBody, courseService.getCourseAPI)
 
-    app.route(BASE_URL + '/read/:courseId')
-        .get(requestMiddleware.createAndValidateRequestBody, courseService.getCourseAPI);
+  app.route(BASE_URL + '/read/mycourse/:createdBy')
+    .get(requestMiddleware.createAndValidateRequestBody, courseService.getMyCourseAPI)
 
-    app.route(BASE_URL + '/read/mycourse/:createdBy')
-        .get(requestMiddleware.createAndValidateRequestBody, courseService.getMyCourseAPI);
+  app.route(BASE_URL + '/hierarchy/:courseId')
+    .get(requestMiddleware.createAndValidateRequestBody, courseService.getCourseHierarchyAPI)
 
-    app.route(BASE_URL + '/hierarchy/:courseId')
-        .get(requestMiddleware.createAndValidateRequestBody, courseService.getCourseHierarchyAPI);
-
-    app.route(BASE_URL + '/retire')
-        .get(contentService.checkHealth);
-};
+  app.route(BASE_URL + '/retire')
+    .get(contentService.checkHealth)
+}
