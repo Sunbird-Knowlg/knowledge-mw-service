b1768e4cd1f50d782b69f1cd033af0f1d3023642
rajeevsathish <rajeev.sathish@tarento.com>
2018-06-08 16:38:12 +0530

    SB-2658 Content-Filtering

--- src/test/middlewares/requestMiddlewareSpec.js
+++ src/test/middlewares/requestMiddlewareSpec.js
@@ -15,15 +15,39 @@ var req = {
     }
   }
 }
+var req1 = {
+  'body': {
+    'request': {
+      'filters': {
+      }
+    }
+  }
+}
+
 describe('filter of channels', function () {
   it('check if request is there then do next', function () {
-    requestMiddleware.addChannelFilters(req, {}, function (obj) {
-      expect(obj.body.request.filters.channel).toMatch('in.ekstep')
+    requestMiddleware.addChannelFilters(req, {}, function () {
+      expect(req.body.request.filters.channel).toMatch('in.ekstep')
     })
   })
   it('check for no request and get config', function () {
     spyOn(filterService, 'getChannelSearchString')
-    requestMiddleware.addChannelFilters({}, {}, function (req) {})
+    requestMiddleware.addChannelFilters(req1, {}, function () {})
     expect(filterService.getChannelSearchString).toHaveBeenCalled()
+  })
+  it('check for getChannelSearchString method creates proper whitelisted search string', function () {
+    var whiteListQuery = (process.env.sunbird_content_service_whitelisted_channels).split(',')
+    requestMiddleware.addChannelFilters(req1, {}, function () {
+      expect(req1.body.request.filters.channel).toEqual(whiteListQuery)
+    })
+  })
+  it('check for getChannelSearchString method creates proper blacklisted search string', function () {
+    var blackListQuery = {'ne': ((process.env.sunbird_content_service_blacklisted_channels).split(','))}
+    var whiteListQuery = (process.env.sunbird_content_service_whitelisted_channels).split(',')
+    requestMiddleware.addChannelFilters(req1, {}, function () {
+      if (!whiteListQuery && blackListQuery) {
+        expect(req1.body.request.filters.channel).toEqual(blackListQuery)
+      }
+    })
   })
 })
