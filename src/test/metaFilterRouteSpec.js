b12a9a57bd7c1cc0a166142036c31b1eb3b148c9
revathi <revathipp@gmail.com>
2018-06-26 10:54:37 +0530

    Issue #SB-3715 fix: review fixes

---
+++ src/test/metaFilterRouteSpec.js
@@ -0,0 +1,4 @@
+describe('Check for all required route to call the AddMetaFilter', function () {
+  it('check for filter in config with route', function () {})
+  it('check for filter in config with value + route', function () {})
+})
---
+++ src/test/middlewares/filterMiddlewareSpec.js
@@ -0,0 +1,9 @@
+describe('Initialization of meta filters', function () {
+  it('check for the request and then do next', function () {})
+  it('check for no request and get config', function () {})
+  it('check for the filter object property exists in the request body', function () {})
+  it('if filter object property is equal to response body property then do not append property to the filter', function () {})
+  it('if the filter object property is not equal to response body property then append property to the filter', function () {})
+  it('check for getMetaSearchData method creates proper whitelisted search data', function () {})
+  it('check for getMetaSearchData method creates proper blacklisted search data', function () {})
+})
