574957ed7e973115baf5ddcd353949d62c2bf502
revathi <revathipp@gmail.com>
2018-06-27 14:41:34 +0530

    Issue #SB-3715 fix: functionality working

--- src/middlewares/filter.middleware.js
+++ src/middlewares/filter.middleware.js
@@ -7,11 +7,41 @@ var filename = path.basename(__filename)
 
 function addMetaFilters (req, res, next) {
   // If the request body has filter by metaFilter data, continue with the same filter, do not alter the values
-  // else call the getMetaSearchData() function to generate the JSON Object for the meta filters
-  filterService.getMetaSearchData(function () {
-    // Generate JSON and return
-    return setFilterJSON()
-  })
+  // else call the getMetaSearchString() function to generate the search string for the meta filters
+  if (req && req.body && req.body.request && req.body.request.filters && req.body.request.filters.channel &&
+    req.body.request.filters.framework && req.body.request.filters.contentType &&
+    req.body.request.filters.mimeType && req.body.request.filters.resourceType) {
+    next()
+  } else {
+    filterService.getMetaSearchString(function (err, searchJSON) {
+      console.log('err', err)
+      console.log('channels', searchJSON)
+      if (err) {
+        LOG.error(utilsService.getLoggerData({}, 'ERROR', filename, 'addChannelFilters',
+          'failed to get channels'))
+      } else if (searchJSON && (!_.isEmpty(searchJSON))) {
+        if (req.body.request.filters.channel === undefined) {
+          req.body.request.filters.channel = searchJSON.channel
+        }
+        if (req.body.request.filters.framework === undefined) {
+          req.body.request.filters.framework = searchJSON.framework
+        }
+        if (req.body.request.filters.contentType === undefined) {
+          req.body.request.filters.contentType = searchJSON.contentType
+        }
+        if (req.body.request.filters.mimeType === undefined) {
+          req.body.request.filters.mimeType = searchJSON.mimeType
+        }
+        if (req.body.request.filters.resourceType === undefined) {
+          req.body.request.filters.resourceType = searchJSON.resourceType
+        }
+        LOG.info(utilsService.getLoggerData({}, 'INFO',
+          filename, 'addChannelFilters', 'added channel filter', req.body.request.filters))
+        console.log('req filters', req.body.request.filters)
+      }
+      next()
+    })
+  }
 }
 
 function setFilterJSON () {
