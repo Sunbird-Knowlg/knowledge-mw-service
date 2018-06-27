28555e6502c769e903810f052593431abf159f57
revathi <revathipp@gmail.com>
2018-06-27 23:36:49 +0530

    Issue #SB-3715 fix: test scenarios and test cases added

--- src/middlewares/filter.middleware.js
+++ src/middlewares/filter.middleware.js
@@ -20,19 +20,20 @@ function addMetaFilters (req, res, next) {
         LOG.error(utilsService.getLoggerData({}, 'ERROR', filename, 'addChannelFilters',
           'failed to get channels'))
       } else if (searchJSON && (!_.isEmpty(searchJSON))) {
-        if (req.body.request.filters.channel === undefined) {
+        if (req.body.request.filters.channel && searchJSON.channel !== undefined) {
           req.body.request.filters.channel = searchJSON.channel
         }
-        if (req.body.request.filters.framework === undefined) {
+        if (req.body.request.filters.framework === undefined && searchJSON.framework !== undefined) {
           req.body.request.filters.framework = searchJSON.framework
         }
-        if (req.body.request.filters.contentType === undefined) {
+
+        if (req.body.request.filters.contentType === undefined && searchJSON.contentType !== undefined) {
           req.body.request.filters.contentType = searchJSON.contentType
         }
-        if (req.body.request.filters.mimeType === undefined) {
+        if (req.body.request.filters.mimeType === undefined && searchJSON.mimeType !== undefined) {
           req.body.request.filters.mimeType = searchJSON.mimeType
         }
-        if (req.body.request.filters.resourceType === undefined) {
+        if (req.body.request.filters.resourceType === undefined && searchJSON.channel !== undefined) {
           req.body.request.filters.resourceType = searchJSON.resourceType
         }
         LOG.info(utilsService.getLoggerData({}, 'INFO',
