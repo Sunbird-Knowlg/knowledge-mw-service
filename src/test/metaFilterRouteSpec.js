5495c42954837c40a7077dfde9f83d6812badb35
revathi <revathipp@gmail.com>
2018-07-03 17:36:58 +0530

    Issue #SB-3715 fix: unit test cases

--- src/test/metaFilterRouteSpec.js
+++ src/test/metaFilterRouteSpec.js
@@ -1,8 +1,206 @@
+// const express = require('express')
+var configUtil = require('../libs/sb-config-util')
+var _ = require('underscore')
+
+const metaFilterRoutes = [
+  'content/search',
+  'course/search',
+  'framework/category/search',
+  'framework/term/search',
+  'search',
+  'framework/list'
+]
+
+const nonFilterRoutes = [
+  'xyz',
+  'abc'
+]
+
+var filterMiddleware = require('../middlewares/filter.middleware')
+var httpMocks = require('node-mocks-http')
+
+var baseUrl = 'http://localhost:5000/v1/'
+var async = require('async')
+
+const whiteListedChannelList = process.env.sunbird_content_service_whitelisted_channels
+const blackListedChannelList = process.env.sunbird_content_service_blacklisted_channels
+
+const whitelistedFrameworkList = process.env.sunbird_content_filter_framework_whitelist
+const blacklistedFrameworkList = process.env.sunbird_content_filter_framework_blacklist
+
+const whitelistedMimeTypeList = process.env.sunbird_content_filter_mimetype_whitelist
+const blacklistedMimeTypeList = process.env.sunbird_content_filter_mimetype_blacklist
+
+const whitelistedContentTypeList = process.env.sunbird_content_filter_contenttype_whitelist
+const blacklistedContentTypeList = process.env.sunbird_content_filter_contenttype_blacklist
+
+const whitelistedResourceTypeList = process.env.sunbird_content_filter_resourcetype_whitelist
+const blacklistedResourceTypeList = process.env.sunbird_content_filter_resourcetype_blacklist
+
+var allowedChannels = whiteListedChannelList ? whiteListedChannelList.split(',') : []
+var blackListedChannels = blackListedChannelList ? blackListedChannelList.split(',') : []
+
+var allowedFramework = whitelistedFrameworkList ? whitelistedFrameworkList.split(',') : []
+var blackListedFramework = blacklistedFrameworkList ? blacklistedFrameworkList.split(',') : []
+var allowedMimetype = whitelistedMimeTypeList ? whitelistedMimeTypeList.split(',') : []
+var blackListedMimetype = blacklistedMimeTypeList ? blacklistedMimeTypeList.split(',') : []
+
+var allowedContenttype = whitelistedContentTypeList ? whitelistedContentTypeList.split(',') : []
+var blackListedContenttype = blacklistedContentTypeList ? blacklistedContentTypeList.split(',') : []
+
+var allowedResourcetype = whitelistedResourceTypeList ? whitelistedResourceTypeList.split(',') : []
+var blackListedResourcetype = blacklistedResourceTypeList ? blacklistedResourceTypeList.split(',') : []
+
+var channelConf = generateConfigString(allowedChannels, blackListedChannels)
+var frameworkConf = generateConfigString(allowedFramework, blackListedFramework)
+var mimeTypeConf = generateConfigString(allowedMimetype, blackListedMimetype)
+var contentTypeConf = generateConfigString(allowedContenttype, blackListedContenttype)
+var resourceTypeConf = generateConfigString(allowedResourcetype, blackListedResourcetype)
+
+var generateJSON = {
+  channel: channelConf,
+  framework: frameworkConf,
+  contentType: contentTypeConf,
+  mimeType: mimeTypeConf,
+  resourceType: resourceTypeConf
+}
+
+var configString = {}
+function generateConfigString (allowedMetadata, blackListedMetadata) {
+  if ((allowedMetadata && allowedMetadata.length > 0) && (blackListedMetadata && blackListedMetadata.length > 0)) {
+    configString = _.difference(allowedMetadata, blackListedMetadata)
+    return configString
+  } else if (allowedMetadata && allowedMetadata.length > 0) {
+    configString = allowedMetadata
+    return configString
+  } else if (blackListedMetadata && blackListedMetadata.length > 0) {
+    configString = { 'ne': blackListedMetadata }
+    return configString
+  }
+}
+
 describe('Check for all required route to call the AddMetaFilter', function () {
-  it('check for filter in config with route', function () {})
-  it('check for filter in config with value + route', function () {})
+  async.forEach(metaFilterRoutes, function (route, callback) {
+    describe('Composite search services', function () {
+      var req, res
+      var body = {
+        'request': {
+          'query': 'Test',
+          'filters': {}
+        }
+      }
+
+      beforeEach(function (done) {
+        req = httpMocks.createRequest({
+          method: 'POST',
+          uri: baseUrl + route,
+          body: body
+        })
+
+        res = httpMocks.createResponse()
+
+        done() // call done so that the next test can run
+      })
+      it('check for filter in config with route', function () {
+        filterMiddleware.addMetaFilters(req, res, function next () {
+          expect(req.body.request.filters).toBeDefined()
+        })
+      })
+      it('check for filter in config with value' + route, function () {
+        const allwhiteListedFilterQuery = {
+          channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+          framework: [ 'NCF' ],
+          contentType: [ 'Resource' ],
+          mimeType: [ 'application/vnd.ekstep.content-collection' ],
+          resourceType: [ 'Learn' ]
+        }
+        configUtil.setConfig('META_FILTER_REQUEST_JSON', allwhiteListedFilterQuery)
+
+        filterMiddleware.addMetaFilters(req, res, function next () {
+          var filterQuery = generateJSON
+
+          // channels
+          if (allowedChannels && allowedChannels.length > 0 && blackListedChannels && blackListedChannels.length > 0) {
+            expect(req.body.request.filters.channel).toEqual(filterQuery.channel)
+          } else if (allowedChannels && allowedChannels.length > 0) {
+            expect(req.body.request.filters.channel).toEqual(allowedChannels)
+          } else if (blackListedChannels && blackListedChannels.length > 0) {
+            expect(req.body.request.filters.channel).toEqual(blackListedChannels)
+          }
+
+          // framework
+          if (allowedFramework && allowedFramework.length > 0 && blackListedFramework &&
+            blackListedFramework.length > 0) {
+            expect(req.body.request.filters.framework).toEqual(filterQuery.framework)
+          } else if (allowedFramework && allowedFramework.length > 0) {
+            expect(req.body.request.filters.framework).toEqual(allowedFramework)
+          } else if (blackListedFramework && blackListedFramework.length > 0) {
+            expect(req.body.request.filters.framework).toEqual(blackListedFramework)
+          }
+
+          // contentType
+          if (allowedContenttype && blackListedContenttype) {
+            expect(req.body.request.filters.contentType).toEqual(filterQuery.contentType)
+          } else if (allowedContenttype && allowedContenttype.length > 0) {
+            expect(req.body.request.filters.contentType).toEqual(allowedContenttype)
+          } else if (blackListedContenttype && blackListedContenttype.length > 0) {
+            expect(req.body.request.filters.contentType).toEqual(blackListedContenttype)
+          }
+
+          // mimeType
+          if (allowedMimetype && blackListedMimetype) {
+            expect(req.body.request.filters.mimeType).toEqual(filterQuery.mimeType)
+          } else if (allowedMimetype && allowedMimetype.length > 0) {
+            expect(req.body.request.filters.mimeType).toEqual(allowedMimetype)
+          } else if (blackListedMimetype && blackListedMimetype.length > 0) {
+            expect(req.body.request.filters.mimeType).toEqual(blackListedMimetype)
+          }
+
+          // resourceType
+          if (allowedResourcetype && blackListedResourcetype) {
+            expect(req.body.request.filters.resourceType).toEqual(filterQuery.resourceType)
+          } else if (allowedResourcetype && allowedResourcetype.length > 0) {
+            expect(req.body.request.filters.resourceType).toEqual(allowedResourcetype)
+          } else if (blackListedResourcetype && blackListedResourcetype.length > 0) {
+            expect(req.body.request.filters.resourceType).toEqual(blackListedResourcetype)
+          }
+        })
+      })
+    })
+  })
 })
 describe('Check for routes not to call the AddMetaFilter', function () {
-  it('if framework filter calls the route, addMetaFilter should not be called ', function () {})
-  it('if framework filter calls the route, filter should not be generated', function () {})
+  // it('if framework filter calls the route, addMetaFilter should not be called ', function () {
+  async.forEach(nonFilterRoutes, function (route, callback) {
+    describe('Composite search services for non filters', function (done) {
+      var req
+      var body = {
+        'request': {
+          'query': 'Test',
+          'filters': {}
+        }
+      }
+      beforeEach(function (done) {
+        req = httpMocks.createRequest({
+          method: 'POST',
+          uri: baseUrl + route,
+          body: body
+        })
+
+        done()
+      })
+      it('if framework filter calls the route, addMetaFilter should not be called  ' + route, function () {
+        const allwhiteListedFilterQuery = {
+          channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+          framework: [ 'NCF' ],
+          contentType: [ 'Resource' ],
+          mimeType: [ 'application/vnd.ekstep.content-collection' ],
+          resourceType: [ 'Learn' ]
+        }
+        configUtil.setConfig('META_FILTER_REQUEST_JSON', allwhiteListedFilterQuery)
+        expect(!_.includes(metaFilterRoutes, route)).toBeTruthy()
+        expect(req.body.request.filters).toEqual({})
+      })
+    })
+  })
 })
--- src/test/middlewares/filterMiddlewareSpec.js
+++ src/test/middlewares/filterMiddlewareSpec.js
@@ -1,39 +1,349 @@
+/**
+ * @name : requestMiddleware.js
+ * @description : Responsible for test request.middleware.js
+ * @author      : Revathi
+ **/
+var filterMiddleware = require('../../middlewares/filter.middleware')
+var filterService = require('../../service/filterService')
+var configUtil = require('../../libs/sb-config-util')
+// var _ = require('underscore')
+
+const req = {
+  'body': {
+    'request': {
+      'filters': {
+        channel: [ 'in.ekstep' ],
+        framework: [ 'NCF' ],
+        contentType: [ 'Resource' ],
+        mimeType: [ 'application/vnd.ekstep.content-collection' ],
+        resourceType: [ 'Learn' ]
+      }
+    }
+  }
+}
+
+const req1 = {
+  'body': {
+    'request': {
+      'filters': {
+      }
+    }
+  }
+}
+
 describe('Initialization of meta filters', function () {
-  it('check for the request with all filter properties and then do next', function () {})
-  it('check for no filter property in request, then call getMetadataFilterQuery() to get config', function () {})
+  it('check for the request with  filters property, else then do next', function () {
+    filterMiddleware.addMetaFilters(req, {}, function () {
+      expect(req.body.request.filters.channel && req.body.request.filters.framework &&
+        req.body.request.filters.contentType &&
+      req.body.request.filters.mimeType && req.body.request.filters.resourceType).toBeDefined()
+    })
+  })
+  it('check for no filter property in request, then call getMetadataFilterQuery() to get config', function () {
+    filterMiddleware.addMetaFilters(req, {}, function () {
+      expect(req.body.request.filters.channel).toMatch('in.ekstep')
+      expect(req.body.request.filters.contentType).toMatch('Resource')
+      expect(req.body.request.filters.framework).toMatch('NCF')
+      expect(req.body.request.filters.mimeType).toMatch('application/vnd.ekstep.content-collection')
+      expect(req.body.request.filters.resourceType).toMatch('Learn')
+    })
+  })
 
-  it('check for the filter object (framework) exists in the request body', function () {})
-  it('if filter object (framework) is equal to response body property then do not append ' +
-  'property to filter', function () {})
-  it('if the filter object (framework) is not equal to response body property then append ' +
-  'property to filter', function () {})
+  it('check for no request and get config', function () {
+    spyOn(filterService, 'getMetadataFilterQuery')
+    filterMiddleware.addMetaFilters(req1, {}, function () {})
+    expect(filterService.getMetadataFilterQuery).toHaveBeenCalled()
+  })
+  it('check for getChannelSearchString method creates proper blacklisted search string', function () {
+    const req1 = {
+      'body': {
+        'request': {
+          'filters': {
+          }
+        }
+      }
+    }
+    var blacklist = {
+      channel: { ne: ['0124758418460180480', '0124758449502453761'] },
+      framework: { ne: [ '01231711180382208027', '012315809814749184151' ] },
+      contentType: { ne: [ 'Story' ] },
+      mimeType: { ne: [ 'application/vnd.ekstep.h5p-archive' ] },
+      resourceType: { ne: [ 'Read' ] }
+    }
 
-  it('check for the filter object (channel) exists in the request body', function () {})
-  it('if filter object (channel) is equal to response body property then do not append ' +
-  'property to filter', function () {})
-  it('if the filter object (channel) is not equal to response body property then append ' +
-  'property to filter', function () {})
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', blacklist)
+    filterMiddleware.addMetaFilters(req1, {}, function () {
+      expect(req1.body.request.filters.channel).toEqual(blacklist.channel)
+    })
+  })
+  it('check for getChannelSearchString method creates proper whitelisted search string', function () {
+    const req1 = {
+      'body': {
+        'request': {
+          'filters': {
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
 
-  it('check for the filter object (mimeType) exists in the request body', function () {})
-  it('if filter object (mimeType) is equal to response body property then do not append ' +
-  'property to filter', function () {})
-  it('if the filter object (mimeType) is not equal to response body property then append ' +
-  'property to filter', function () {})
+    filterMiddleware.addMetaFilters(req1, {}, function () {
+      expect(req1.body.request.filters.channel).toEqual(whiteList.channel)
+      expect(req1.body.request.filters.framework).toEqual(whiteList.framework)
+      expect(req1.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req1.body.request.filters.mimeType).toEqual(whiteList.mimeType)
+      expect(req1.body.request.filters.resourceType).toEqual(whiteList.resourceType)
+    })
+  })
 
-  it('check for the filter object (resourceType) exists in the request body', function () {})
-  it('if filter object (resourceType) is equal to response body property then do not append ' +
-  'property to filter', function () {})
-  it('if the filter object (resourceType) is not equal to response body property then append ' +
-  'property to filter', function () {})
+  it('if the filter object (channel) exists in req,  then do not append res channel property to filter', function () {
+    const req = {
+      'body': {
+        'request': {
+          'filters': {
+            channel: [ 'in.ekstep' ],
+            framework: [ 'NCF' ],
+            contentType: [ 'Resource' ],
+            mimeType: [ 'application/vnd.ekstep.content-collection' ],
+            resourceType: [ 'Learn' ]
+          }
+        }
+      }
+    }
+    filterMiddleware.addMetaFilters(req, {}, function () {
+      if (req && req.body && req.body.request && req.body.request.filters.channel) {
+        expect(req.body.request.filters.channel).toBeDefined()
+      }
+    })
+  })
+  it('if the filter object (framework) exists in req, then do not append res framework to filter', function () {
+    if (req && req.body && req.body.request && req.body.request.filters.framework) {
+      expect(req.body.request.filters.framework).toBeDefined()
+    }
+  })
 
-  it('check for the filter object (contentType) exists in the request body', function () {})
-  it('if filter object (contentType) is equal to response body property then do not append ' +
-  'property to filter', function () {})
-  it('if the filter object (contentType) is not equal to response body property then append ' +
-  'property to filter', function () {})
+  it('if the filter object (mimeType) exists in req, then do not append res mimeType to filter', function () {
+    if (req && req.body && req.body.request && req.body.request.filters.mimeType) {
+      expect(req.body.request.filters.mimeType).toBeDefined()
+    }
+  })
 
-  // negative
-  it('if the request filter object (contentType) is equal to response body property then do next()', function () {})
-  it('if the request filter object (contentType) is not equal to response body property then do next()', function () {})
-  it('if the request filter object (contentType) is defined, and response body is undefined, then do next()', function () {})
+  it('if the filter object (contentType) exists in req, then do not append res contentType to filter', function () {
+    if (req && req.body && req.body.request && req.body.request.filters.contentType) {
+      expect(req.body.request.filters.contentType).toBeDefined()
+    }
+  })
+
+  it('if the filter object (resourceType) exists in req, then do not append res resourceType to filter', function () {
+    if (req && req.body && req.body.request && req.body.request.filters.resourceType) {
+      expect(req.body.request.filters.resourceType).toBeDefined()
+    }
+  })
+})
+
+// negative Scenarios
+describe(' Combination of 2 meta filters  defined ', function () {
+  it('if only channel, framework is configured, form filter from Env Config service', function () {
+    const req1 = {
+      'body': {
+        'request': {
+          'filters': {
+            channel: [ 'in.ekstep' ],
+            framework: [ 'NCFCOPY' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req1, {}, function () {
+      expect(req1.body.request.filters.channel).toBeDefined()
+      expect(req1.body.request.filters.framework).toBeDefined()
+      expect(req1.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req1.body.request.filters.mimeType).toEqual(whiteList.mimeType)
+      expect(req1.body.request.filters.resourceType).toEqual(whiteList.resourceType)
+    })
+  })
+  it('if only framework and mimeType is configured, form filter from Env Config service', function () {
+    const req2 = {
+      'body': {
+        'request': {
+          'filters': {
+            framework: [ 'NCFCOPY' ],
+            mimeType: [ 'application/vnd.ekstep.h5p-archive' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req2, {}, function () {
+      expect(req2.body.request.filters.mimeType).toBeDefined()
+      expect(req2.body.request.filters.framework).toBeDefined()
+      expect(req2.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req2.body.request.filters.channel).toEqual(whiteList.channel)
+      expect(req2.body.request.filters.resourceType).toEqual(whiteList.resourceType)
+    })
+  })
+  it('if only framework and resourceType is configured', function () {
+    const req3 = {
+      'body': {
+        'request': {
+          'filters': {
+            framework: [ 'NCFCOPY' ],
+            resourceType: [ 'Learn' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req3, {}, function () {
+      expect(req3.body.request.filters.resourceType).toBeDefined()
+      expect(req3.body.request.filters.framework).toBeDefined()
+      expect(req3.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req3.body.request.filters.channel).toEqual(whiteList.channel)
+      expect(req3.body.request.filters.resourceType).toEqual(whiteList.resourceType)
+    })
+  })
+  it('if channel and resourceType is configured', function () {
+    const req4 = {
+      'body': {
+        'request': {
+          'filters': {
+            channel: [ 'in.ekstep' ],
+            resourceType: [ 'Story' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req4, {}, function () {
+      expect(req4.body.request.filters.resourceType).toBeDefined()
+      expect(req4.body.request.filters.channel).toBeDefined()
+      expect(req4.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req4.body.request.filters.framework).toEqual(whiteList.framework)
+      expect(req4.body.request.filters.mimeType).toEqual(whiteList.mimeType)
+    })
+  })
+  it('if contentType and mimeType is configured', function () {
+    const req5 = {
+      'body': {
+        'request': {
+          'filters': {
+            contentType: [ 'in.ekstep' ],
+            mimeType: [ 'Story' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req5, {}, function () {
+      expect(req5.body.request.filters.contentType).toBeDefined()
+      expect(req5.body.request.filters.mimeType).toBeDefined()
+      expect(req5.body.request.filters.channel).toEqual(whiteList.channel)
+      expect(req5.body.request.filters.framework).toEqual(whiteList.framework)
+      expect(req5.body.request.filters.resourceType).toEqual(whiteList.resourceType)
+    })
+  })
+  it('if contentType and resourceType is configured', function () {
+    const req6 = {
+      'body': {
+        'request': {
+          'filters': {
+            contentType: [ 'Lesson' ],
+            resourceType: [ 'Story' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req6, {}, function () {
+      expect(req6.body.request.filters.contentType).toBeDefined()
+      expect(req6.body.request.filters.resourceType).toBeDefined()
+      expect(req6.body.request.filters.channel).toEqual(whiteList.channel)
+      expect(req6.body.request.filters.framework).toEqual(whiteList.framework)
+      expect(req6.body.request.filters.mimeType).toEqual(whiteList.mimeType)
+    })
+  })
+  it('if channel and resourceType is configured', function () {
+    const req7 = {
+      'body': {
+        'request': {
+          'filters': {
+            channel: [ 'in.ekstep' ],
+            resourceType: [ 'Story' ]
+          }
+        }
+      }
+    }
+    var whiteList = {
+      channel: ['b00bc992ef25f1a9a8d63291e20efc8d'],
+      framework: [ 'NCF' ],
+      contentType: [ 'Resource' ],
+      mimeType: [ 'application/vnd.ekstep.content-collection' ],
+      resourceType: [ 'Learn' ]
+    }
+    configUtil.setConfig('META_FILTER_REQUEST_JSON', whiteList)
+
+    filterMiddleware.addMetaFilters(req7, {}, function () {
+      expect(req7.body.request.filters.channel).toBeDefined()
+      expect(req7.body.request.filters.resourceType).toBeDefined()
+      expect(req7.body.request.filters.contentType).toEqual(whiteList.contentType)
+      expect(req7.body.request.filters.framework).toEqual(whiteList.framework)
+      expect(req7.body.request.filters.mimeType).toEqual(whiteList.mimeType)
+    })
+  })
 })
