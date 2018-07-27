c9dc3c7bd4db6720cf713372dc20301b9d25d86c
loganathan.shanmugam@tarento.com <loganathan.shanmugam@tarento.com>
2018-07-27 11:29:31 +0530

    Issue #SB-4854 fix: resolved conflicts

--- src/test/helpers/configHelperSpec.js
+++ src/test/helpers/configHelperSpec.js
@@ -1,20 +1,18 @@
 var configHelper = require('../../helpers/configHelper')
-var configUtil = require('sb-config-util')
 
-describe('configuration settings', function () {
-  it('should set the configuration ', function (done) {
+describe('configuration helper methods', function () {
+  it('should set the configuration ', function () {
+    spyOn(configHelper, 'getFilterConfig')
     configHelper.updateConfig()
-    expect(configUtil.getConfig('CHANNEL_FILTER_QUERY_STRING')).toEqual([])
-    done()
+    expect(configHelper.getFilterConfig).toHaveBeenCalled()
   })
 })
 describe('configuration string', function () {
-  it('should return the config string with all channels', function (done) {
+  it('should return the config string with all channels when $.instance.all is present', function (done) {
     process.env.sunbird_content_service_whitelisted_channels =
     '505c7c48ac6dc1edc9b08f21db5a571d,b00bc992ef25f1a9a8d63291e20efc8d,$.instance.all'
     process.env.sunbird_content_service_blacklisted_channels = 'b00bc992ef25f1a9a8d63291e20efc8d'
-    configHelper.getFilterConfig(function (err, configStr) {
-      expect(err).toBeNull()
+    configHelper.getFilterConfig().then(function (configStr) {
       expect(configStr.length).toBeGreaterThanOrEqual(process.env.sunbird_content_service_whitelisted_channels.length)
       done()
     })
@@ -23,8 +21,7 @@ describe('configuration string', function () {
     process.env.sunbird_content_service_whitelisted_channels =
     '505c7c48ac6dc1edc9b08f21db5a571d,b00bc992ef25f1a9a8d63291e20efc8d'
     process.env.sunbird_content_service_blacklisted_channels = 'b00bc992ef25f1a9a8d63291e20efc8d'
-    configHelper.getFilterConfig(function (err, configStr) {
-      expect(err).toBeNull()
+    configHelper.getFilterConfig().then(function (configStr) {
       expect(configStr.length).toBeLessThanOrEqual(process.env.sunbird_content_service_whitelisted_channels.length)
       done()
     })
