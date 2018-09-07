f352d1713a963849334f030c5aabc34bf3ee5b8e
Sourav Dey <sourav_d@techjoomla.com>
2018-09-07 15:32:00 +0530

    Issue #SB-4561 feat: email service change

--- src/service/emailService.js
+++ src/service/emailService.js
@@ -275,6 +275,7 @@ function getUserDetails (req) {
         }
       }
     }
+    console.log(data)
     LOG.info(utilsService.getLoggerData(req.rspObj, 'INFO', filename, 'getUserDetails headers info',
       'getUserDetails headers', req.headers))
     contentProvider.userSearch(data, req.headers, function (err, result) {
@@ -449,6 +450,7 @@ function publishedContentEmail (req, callback) {
  * @param {function} callback
  */
 function reviewContentEmail (req, callback) {
+  console.log('-------------------reviewContentAPI email service came')
   if (!req.params.contentId) {
     LOG.error(utilsService.getLoggerData(req.rspObj, 'ERROR', filename, 'sendForReview',
       'Content id is missing', null))
