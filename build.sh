4c697b4b2a15c202351b26f481f1dab59afcd7a8
Keshav Prasad <msknext@gmail.com>
2019-01-07 15:12:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # set -o errexit
 commit_hash=$1
 name=content-service
-version=$2
 node=$3
 org=$4
 
@@ -12,5 +11,5 @@ docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
 containerid=`docker ps -aqf "name=${name}-${version}-build"`
 docker cp $containerid:/opt/content_service.zip content_service.zip
 docker rm $containerid
-docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${version}_${commit_hash} .
+docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${commit_hash} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${version}_${commit_hash}\", \"node_name\" : \"$node\"} > metadata.json
