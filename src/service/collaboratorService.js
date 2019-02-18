d3906a375b17373140a6a150e51634c3a5ee197b
yravinderkumar33 <yravinderkumar33@gmail.com>
2019-02-18 12:13:57 +0530

    Issue #Sb-10299 feat : logs added in collaboratorsService

--- src/service/collaboratorService.js
+++ src/service/collaboratorService.js
@@ -8,7 +8,7 @@ var async = require('async')
 var path = require('path')
 var contentProvider = require('sb_content_provider_util')
 var respUtil = require('response_util')
-var LOG = require('sb_logger_util')
+var logger = require('sb_logger_util_v2')
 var validatorUtil = require('sb_req_validator_util')
 var configUtil = require('sb-config-util')
 
@@ -34,11 +34,18 @@ function updateCollaborators (req, response) {
 
   if (!data.request || !data.request.content ||
     !validatorUtil.validate(data.request.content, contentModel.COLLABORATORS)) {
-    LOG.error(utilsService.getLoggerData(rspObj, 'ERROR', filename, 'updateCollaboratorsAPI',
-      'Error due to required params are missing', data.request))
     rspObj.errCode = contentMessage.COLLABORATORS.MISSING_CODE
     rspObj.errMsg = contentMessage.COLLABORATORS.MISSING_MESSAGE
     rspObj.responseCode = responseCode.CLIENT_ERROR
+    logger.error({
+      msg: 'Error due to required request body || content is missing',
+      err: {
+        errCode: rspObj.errCode,
+        errMsg: rspObj.errMsg,
+        responseCode: rspObj.responseCode
+      },
+      additionalInfo: { data }
+    }, req)
     return response.status(400).send(respUtil.errorResponse(rspObj))
   }
 
@@ -49,19 +56,29 @@ function updateCollaborators (req, response) {
         mode: 'edit',
         fields: 'versionKey, collaborators, contentType, name, mimeType, framework, status'
       }
-      LOG.info(utilsService.getLoggerData(rspObj, 'INFO', filename, 'updateCollaboratorsAPI',
-        'Request to content provider to get the latest version key', {
+
+      logger.info({
+        msg: 'Request to update collaborators',
+        additionalInfo: {
           contentId: data.contentId,
-          query: qs,
-          headers: req.headers
-        }))
+          query: qs
+        }
+      }, req)
       contentProvider.getContentUsingQuery(data.contentId, qs, req.headers, function (err, res) {
         if (err || res.responseCode !== responseCode.SUCCESS) {
-          LOG.error(utilsService.getLoggerData(rspObj, 'ERROR', filename, 'updateCollaboratorsAPI',
-            'Getting error from content provider', res))
           rspObj.errCode = res && res.params ? res.params.err : contentMessage.COLLABORATORS.FAILED_CODE
           rspObj.errMsg = res && res.params ? res.params.errmsg : contentMessage.COLLABORATORS.FAILED_MESSAGE
           rspObj.responseCode = res && res.responseCode ? res.responseCode : responseCode.SERVER_ERROR
+          logger.error({
+            msg: 'Getting error from content provider while fetching content',
+            err: {
+              err,
+              errCode: rspObj.errCode,
+              errMsg: rspObj.errMsg,
+              responseCode: rspObj.responseCode
+            },
+            additionalInfo: { data }
+          }, req)
           var httpStatus = res && res.statusCode >= 100 && res.statusCode < 600 ? res.statusCode : 500
           rspObj = utilsService.getErrorResponse(rspObj, res)
           return response.status(httpStatus).send(respUtil.errorResponse(rspObj))
@@ -71,17 +88,29 @@ function updateCollaborators (req, response) {
             rspObj.errMsg = contentMessage.COLLABORATORS.FAILED_MESSAGE
             rspObj.responseCode = res.result.content.status === 'Retired'
               ? responseCode.RESOURCE_NOT_FOUND : contentMessage.COLLABORATORS.FORBIDDEN
+
+            logger.error({
+              msg: 'Cannot update collaborators because content is in Draft state',
+              err: {
+                errCode: rspObj.errCode,
+                errMsg: rspObj.errMsg,
+                responseCode: rspObj.responseCode
+              }
+            }, req)
+
             httpStatus = res.result.content.status === 'Retired'
               ? 404 : 403
             return response.status(httpStatus).send(respUtil.errorResponse(rspObj))
           }
           data.request.content.versionKey = res.result.content.versionKey
           data.request.content.collaborators = lodash.uniq(data.request.content.collaborators)
-          var contentInfo = { contentTitle: res.result.content.name,
+          var contentInfo = {
+            contentTitle: res.result.content.name,
             contentType: res.result.content.contentType,
             identifier: data.contentId,
             mimeType: res.result.content.mimeType,
-            framework: res.result.content.framework }
+            framework: res.result.content.framework
+          }
           compareCollaborators(req, contentInfo, res.result.content.collaborators, data.request.content.collaborators)
           CBW()
         }
@@ -91,18 +120,27 @@ function updateCollaborators (req, response) {
       var ekStepReqData = {
         request: data.request
       }
-      LOG.info(utilsService.getLoggerData(rspObj, 'INFO', filename, 'updateCollaboratorsAPI',
-        'Request to content provider to update the content', {
-          body: ekStepReqData,
-          headers: req.headers
-        }))
+      logger.info({
+        msg: 'Request to content provider to update the content',
+        additionalInfo: {
+          body: ekStepReqData
+        }
+      }, req)
       contentProvider.updateContent(ekStepReqData, data.contentId, req.headers, function (err, res) {
         if (err || res.responseCode !== responseCode.SUCCESS) {
-          LOG.error(utilsService.getLoggerData(rspObj, 'ERROR', filename, 'updateCollaboratorsAPI',
-            'Getting error from content provider', res))
           rspObj.errCode = res && res.params ? res.params.err : contentMessage.COLLABORATORS.FAILED_CODE
           rspObj.errMsg = res && res.params ? res.params.errmsg : contentMessage.COLLABORATORS.FAILED_MESSAGE
           rspObj.responseCode = res && res.responseCode ? res.responseCode : responseCode.SERVER_ERROR
+          logger.error({
+            msg: 'Getting error from content provider while updating content',
+            err: {
+              err,
+              errCode: rspObj.errCode,
+              errMsg: rspObj.errMsg,
+              responseCode: rspObj.responseCode
+            },
+            additionalInfo: { data }
+          }, req)
           var httpStatus = res && res.statusCode >= 100 && res.statusCode < 600 ? res.statusCode : 500
           rspObj = utilsService.getErrorResponse(rspObj, res)
           return response.status(httpStatus).send(respUtil.errorResponse(rspObj))
@@ -115,8 +153,13 @@ function updateCollaborators (req, response) {
     function (res) {
       rspObj.result.content_id = res.result.node_id
       rspObj.result.versionKey = res.result.versionKey
-      LOG.info(utilsService.getLoggerData(rspObj, 'INFO', filename, 'updateCollaboratorsAPI',
-        'Sending response back to user', rspObj))
+      logger.info({
+        msg: 'updateCollaborators API result',
+        additionalInfo: {
+          contentId: rspObj.result.content_id,
+          versionKey: rspObj.result.versionKey
+        }
+      }, req)
       return response.status(200).send(respUtil.successResponse(rspObj))
     }
   ])
@@ -159,14 +202,11 @@ function notifyCollaborators (req, cData, collaboratorsArray, emailType) {
         var lsEmailData = {
           request: getEmailData(subject, body, collaboratorsArray, eData.TEMPLATE)
         }
-        LOG.info(utilsService.getLoggerData(rspObj, 'INFO', filename, emailType,
-          'Request to Leaner service to send email', {
-            body: lsEmailData
-          }))
+
+        logger.info({ msg: 'request to send mail', additionalInfo: { body: lsEmailData } }, req)
         contentProvider.sendEmail(lsEmailData, req.headers, function (err, res) {
           if (err || res.responseCode !== responseCode.SUCCESS) {
-            LOG.error(utilsService.getLoggerData(rspObj, 'ERROR', filename, emailType,
-              'Sending email failed', res))
+            logger.error({ msg: 'Sending email failed', err }, req)
             CBW(new Error('Sending email failed'), null)
           } else {
             CBW(null, res)
@@ -175,8 +215,7 @@ function notifyCollaborators (req, cData, collaboratorsArray, emailType) {
       },
 
       function (res) {
-        LOG.info(utilsService.getLoggerData(rspObj, 'INFO', filename, emailType,
-          'Email sent successfully', rspObj))
+        logger.info({ msg: 'Email sent successfully' }, req)
         return (null, true)
       }
     ])
@@ -191,7 +230,7 @@ function getContentUrl (content) {
   var baseUrl = configUtil.getConfig('SUNBIRD_PORTAL_BASE_URL') + '/workspace/content/edit'
   if (content.mimeType === 'application/vnd.ekstep.content-collection') {
     return baseUrl + '/collection/' + content.identifier + '/' + content.contentType +
-    '/collaborating-on/' + content.framework + '/Draft'
+      '/collaborating-on/' + content.framework + '/Draft'
   } else if (content.mimeType === 'application/vnd.ekstep.ecml-archive') {
     return baseUrl + '/content/' + content.identifier + '/collaborating-on/' + content.framework + '/Draft'
   } else {
