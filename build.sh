e70594d1717312f64189f54e06b43c2faac1e935
G33tha <geetha.p@optit.co>
2019-02-23 16:43:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 # Build script
 # set -o errexit
-e () {
-    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
-}
-m=$(./src/metadata.sh)
+build_tag=$1
+name=content-service
+node=$2
+org=$3
 
-org=$(e "${m}" "org")
-name=$(e "${m}" "name")
-version=$(e "${m}" "version")
-
-
-docker build -f ./Dockerfile.Build -t ${org}/${name}:${version}-build . 
-docker run --name=${name}-${version}-build ${org}/${name}:${version}-build 
-containerid=`docker ps -aqf "name=${name}-${version}-build"`
+docker build -f ./Dockerfile.Build -t ${org}/${name}:${build_tag}-build . 
+docker run --name=${name}-${build_tag}-build ${org}/${name}:${build_tag}-build 
+containerid=$(docker ps -aqf "name=${name}-${build_tag}-build")
 docker cp $containerid:/opt/content_service.zip content_service.zip
 docker rm $containerid
-docker build -f ./Dockerfile -t ${org}/${name}:${version}-bronze .
+docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
