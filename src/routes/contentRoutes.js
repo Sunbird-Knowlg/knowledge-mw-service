61c5ad07693bc8af506c9f9544b90c76545fa013
Sourav Dey <sourav_d@techjoomla.com>
2019-04-09 18:07:55 +0530

    Issue #SB-11587 feat: Gzip of Content Service API responses

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -15,50 +15,59 @@ var dependentServiceHealth = ['EKSTEP']
 module.exports = function (app) {
   app.route(BASE_URL + '/search')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
-      // requestMiddleware.gzipCompression(),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, filterMiddleware.addMetaFilters,
       contentService.searchContentAPI)
 
   app.route(BASE_URL + '/create')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, contentService.createContentAPI)
 
   app.route(BASE_URL + '/update/:contentId')
     .patch(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForCreatorUser, contentService.updateContentAPI)
 
   app.route(BASE_URL + '/upload/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForCreatorUser, contentService.uploadContentAPI)
 
   app.route(BASE_URL + '/review/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForCreatorUser, contentService.reviewContentAPI)
 
   app.route(BASE_URL + '/publish/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForReviewerUser, contentService.publishContentAPI)
 
   app.route(BASE_URL + '/retire')
     .delete(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       contentService.retireContentAPI)
 
   app.route(BASE_URL + '/reject/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForReviewerUser, contentService.rejectContentAPI)
 
   app.route(BASE_URL + '/read/:contentId')
     .get(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, contentService.getContentAPI)
 
   app.route(BASE_URL + '/read/mycontent/:createdBy')
     .get(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, contentService.getMyContentAPI)
 
   app.route(BASE_URL + '/flag/:contentId')
@@ -67,6 +76,7 @@ module.exports = function (app) {
 
   app.route(BASE_URL + '/flag/accept/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForReviewerUser, contentService.acceptFlagContentAPI)
 
@@ -77,19 +87,23 @@ module.exports = function (app) {
 
   app.route(BASE_URL + '/upload/url/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       requestMiddleware.apiAccessForCreatorUser, contentService.uploadContentUrlAPI)
 
   app.route(BASE_URL + '/badge/assign/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, contentService.assignBadgeAPI)
 
   app.route(BASE_URL + '/badge/revoke/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, contentService.revokeBadgeAPI)
 
   app.route(BASE_URL + '/copy/:contentId')
     .post(healthService.checkDependantServiceHealth(dependentServiceHealth),
+      requestMiddleware.gzipCompression(),
       requestMiddleware.createAndValidateRequestBody, requestMiddleware.validateToken,
       contentService.copyContentAPI)
 
