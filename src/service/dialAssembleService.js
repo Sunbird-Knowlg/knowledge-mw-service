422fd58f16b020d941a51d7c038e50502a4da82e
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2019-05-22 21:36:03 +0530

    update

--- src/service/dialAssembleService.js
+++ src/service/dialAssembleService.js
@@ -9,108 +9,98 @@ const async = require('async');
 const _ = require('underscore');
 var configUtil = require('sb-config-util');
 
-class PageService {
-    constructor(pageConfig) {
-        this.pageAssembleConfig = {};
-        console.log('this.pageAssembleConfig', this.pageAssembleConfig);
-        let instance = this;
-        pageConfig.page.forEach(function(page) {
-            instance.pageAssembleConfig[page.name] = {
-                web: page.portalSections,
-                app: page.appSections
-            }
-        })
-    }
-    assemble(req, res) {
-        const rspObj = req.rspObj;
-        const source = req.body.request.source;
-        const name = req.body.request.name;
-        const filters = req.body.request.filters;
-        const sections = this.pageAssembleConfig[name][source];
-        let sectionCalls = [];
-        let instance = this;
-        rspObj.result.sections = [];
-        rspObj.result.id = this.pageAssembleConfig[name].id;
-        rspObj.result.name = name;
-        sections.forEach(function(section) {
-            sectionCalls.push(function(callback) {
-                var options = instance.getHttpOptions(configUtil.getConfig('SEARCH_SERVICE_BASE_URL') + configUtil.getConfig('SEARCH_URI'), instance.getSearchRequestBody(JSON.parse(section.searchQuery), filters), 'POST', {})
-                instance.sendRequest(options, function(error, response, body) {
-                    if(error) {
-                        callback(error)
-                    } else {
-                        _.extend(section, response.body.result);
-                        section.apiId = response.body.id;
-                        section.resmsgid = response.body.params.resmsgid;
-                        section.contents = response.body.result.content;
-                        section.content = undefined;
-                        callback(null, section);
-                    }
-                })
-            });
-        })
-        async.parallel(sectionCalls,
-            function(err, results) {
-                if(err) {
-                    instance.sendError(res, { rspObj: rspObj, params: { err: err }});
+function assemble(req, res) {
+    const rspObj = req.rspObj;
+    const source = req.body.request.source;
+    const name = req.body.request.name;
+    var filters = req.body.request.filters;
+    var sections = pageAssembleConfig[name][source];
+    let sectionCalls = [];
+    rspObj.result.sections = [];
+    rspObj.result.id = pageAssembleConfig[name].id;
+    rspObj.result.name = name;
+    sections.forEach(function(section) {
+        sectionCalls.push(function(callback) {
+            var options = getHttpOptions(configUtil.getConfig('SEARCH_SERVICE_BASE_URL') + configUtil.getConfig('SEARCH_URI'), getSearchRequestBody(JSON.parse(section.searchQuery), filters), 'POST', {})
+            sendRequest(options, function(error, response, body) {
+                if(error) {
+                    callback(error)
                 } else {
-                    instance.sendSuccess(res, { results: results, rspObj: rspObj});
+                    _.extend(section, response.body.result);
+                    section.apiId = response.body.id;
+                    section.resmsgid = response.body.params.resmsgid;
+                    section.contents = response.body.result.content;
+                    section.content = undefined;
+                    callback(null, section);
                 }
-            }
-        ); 
-    }
-    getSearchRequestBody(sectionQuery, filters) {
-        if (_.isEmpty(filters))
-            return sectionQuery;
-        var sectionFilters = sectionQuery.request.filters;
-        _.extend(sectionFilters, filters)
-        console.log(JSON.stringify(sectionQuery));
-        return sectionQuery;
-    }
-    sendError(res, options) {
-        const resObj = options.rspObj;
-        resObj.responseCode = options.responseCode || 'SERVER_ERROR';
-        resObj.params = options.params || {};
-        res.status(500);
-        res.json(resObj);
-    }
-    sendSuccess(res, options) {
-        const resObj = options.rspObj;
-        resObj.result.sections = options.results;
-        res.status(200);
-        res.json(resObj);
-    }
-    sendRequest(options, cb) {
-        request(options, function(error, response, body) {
-            cb(error, response, body);
+            })
         });
-    }
-    getHttpOptions(url, data, method, headers) {
-        var defaultHeaders = {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkMTc1MDIwNDdlODc0ODZjOTM0ZDQ1ODdlYTQ4MmM3MyJ9.7LWocwCn5rrCScFQYOne8_Op2EOo-xTCK5JCFarHKSs'
+    })
+    async.parallel(sectionCalls,
+        function(err, results) {
+            if(err) {
+                sendError(res, { rspObj: rspObj, params: { err: err }});
+            } else {
+                sendSuccess(res, { results: results, rspObj: rspObj});
+            }
         }
-      
-        var http_options = {
-          url: url,
-          forever: true,
-          headers: defaultHeaders,
-          method: method,
-          json: true
-        }
-      
-        if (headers) {
-          headers['Content-Type'] = headers['Content-Type'] ? headers['Content-Type'] : defaultHeaders['Content-Type']
-          headers['Authorization'] = defaultHeaders['Authorization']
-          http_options.headers = headers
-        }
-      
-        if (data) { http_options.body = data }
-        return http_options
-    }
+    ); 
+
 }
 
-const pageConfig = {
+function getSearchRequestBody(sectionQuery, filters) {
+    if (_.isEmpty(filters))
+        return sectionQuery;
+    var sectionFilters = sectionQuery.request.filters;
+    _.extend(sectionFilters, filters)
+    return sectionQuery;
+}
+
+function sendError(res, options) {
+    var resObj = options.rspObj;
+    resObj.responseCode = options.responseCode || 'SERVER_ERROR';
+    resObj.params = options.params || {};
+    res.status(500);
+    res.json(resObj);
+}
+
+function sendSuccess(res, options) {
+    var resObj = options.rspObj;
+    resObj.result.sections = options.results;
+    res.status(200);
+    res.json(resObj);
+}
+
+function sendRequest(options, cb) {
+    request(options, function(error, response, body) {
+        cb(error, response, body);
+    });
+}
+
+function getHttpOptions(url, data, method, headers) {
+    var defaultHeaders = {
+      'Content-Type': 'application/json'
+    }
+  
+    var http_options = {
+      url: url,
+      forever: true,
+      headers: defaultHeaders,
+      method: method,
+      json: true
+    }
+  
+    if (headers) {
+      headers['Content-Type'] = headers['Content-Type'] ? headers['Content-Type'] : defaultHeaders['Content-Type']
+      headers['Authorization'] = defaultHeaders['Authorization']
+      http_options.headers = headers
+    }
+  
+    if (data) { http_options.body = data }
+    return http_options
+}
+
+var pageConfig = {
     "page": [
         {
             "name": "DIAL Code Consumption",
@@ -136,4 +126,13 @@ const pageConfig = {
     ]
 }
 
-module.exports = new PageService(pageConfig);
+var pageAssembleConfig = {};
+
+pageConfig.page.forEach(function(page) {
+    pageAssembleConfig[page.name] = {
+        web: page.portalSections,
+        app: page.appSections
+    }
+})
+
+module.exports.assemble = assemble
