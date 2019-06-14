ecbdcc7b5d57f86f5a8be8a8935f0636bbc30eb0
rjshrjndrn <rjshrjndrn@gmail.com>
2019-06-14 12:43:14 +0530

    Issue #000 feat: Updating docker node version to 8.11-slim

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ name=content-service
 node=$2
 org=$3
 
-docker build -f ./Dockerfile.Build -t ${org}/${name}:${build_tag}-build . 
-docker run --name=${name}-${build_tag}-build ${org}/${name}:${build_tag}-build 
-containerid=$(docker ps -aqf "name=${name}-${build_tag}-build")
-docker cp $containerid:/opt/content_service.zip content_service.zip
-docker rm $containerid
+[[ -f ImageMagick-i386-pc-solaris2.11.tar.gz ]] || wget https://www.imagemagick.org/download/binaries/ImageMagick-i386-pc-solaris2.11.tar.gz
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
