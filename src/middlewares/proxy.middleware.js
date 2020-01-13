ccc16cbf315dde63076aad80f015a824e1ab1c88
Pradyumna Nagendra <pradyumnan@ilimi.in>
2020-01-13 12:09:55 +0530

    Issue #SB-15123 feat: itemset APIs proxy

--- src/middlewares/proxy.middleware.js
+++ src/middlewares/proxy.middleware.js
@@ -31,7 +31,7 @@ module.exports = function (app) {
   )
 
   app.use(
-    '/itemset/*',
+    '/api/itemset/*',
     requestMiddleware.validateUserToken,
     proxy(assessmentServiceBaseUrl, {
       limit: reqDataLimitOfContentUpload,
