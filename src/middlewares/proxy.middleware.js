9e3f66017a13821ccacbb08d65a9457b6a87db1b
amitpriyadarshi <darshi.1983@gmail.com>
2020-02-13 16:19:33 +0530

    Issue #SC-1651 feat: UpdateHierarchy api refactor

--- src/middlewares/proxy.middleware.js
+++ src/middlewares/proxy.middleware.js
@@ -279,7 +279,7 @@ module.exports = function (app) {
     })
   )
   app.use(
-    ['/action/framework/v3/read/*', '/action/content/v3/read/*', '/action/content/v1/read/*'],
+    ['/action/framework/v3/read/*', '/action/content/v3/read/*', '/action/content/v1/read/*', '/action/content/v3/hierarchy/update'],
     proxy(contentRepoBaseUrl, {
       limit: reqDataLimitOfContentUpload,
       proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
