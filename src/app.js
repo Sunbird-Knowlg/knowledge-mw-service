cdbeebfeea78bf2b38d7cc07477ee88021fe4e5d
sudip mukherjee <sudip.mukherjee@tarento.com>
2020-12-15 12:56:31 +0530

    Issue #SB-21181 fix: Added alternative directory for the local keys

--- src/app.js
+++ src/app.js
@@ -60,9 +60,9 @@ const isHealthCheckEnabled = process.env.sunbird_health_check_enable || 'true'
 const contentServiceLocalBaseUrl = process.env.sunbird_content_service_local_base_url ? process.env.sunbird_content_service_local_base_url : 'http://knowledge-mw-service:5000'
 const sunbirdGzipEnable = process.env.sunbird_gzip_enable || 'true'
 
-const kidTokenPublicKeyBasePath = process.env.sunbird_kid_public_key_base_path
+const kidTokenPublicKeyBasePath = process.env.sunbird_kid_public_key_base_path || '/keys/'
 
-configUtil.setContentProviderApi(contentProviderApiConfig.API)
+configUtil.setContentProviderApi(contentProviderApiConfig.API) 
 configUtil.setConfig('CONTENT_SERVICE_BASE_URL', contentServiceBaseUrl)
 configUtil.setConfig('CONTENT_SERVICE_AUTH_TOKEN', contentServiceAuthToken)
 configUtil.setConfig('ASSESSMENT_SERVICE_BASE_URL', assessmentServiceBaseUrl)
