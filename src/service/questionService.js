44bb007a0dd402e8cfe9a8b006e08fb2f2479796
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-05-28 11:31:42 +0530

    #SB-23019 feat:Change all logs to follow the standard log format

--- src/service/questionService.js
+++ src/service/questionService.js
@@ -6,12 +6,22 @@
 
 var respUtil = require('response_util')
 var contentProviderUtil = require('sb_content_provider_util')
-var logger = require('sb_logger_util_v2')
 var messageUtils = require('./messageUtil')
 var utilsService = require('./utilsService')
 var _ = require('lodash')
 var responseCode = messageUtils.RESPONSE_CODE
-const questionAllFields = "name,code,description,mimeType,primaryCategory,additionalCategories,visibility,copyright,license,lockKey,assets,audience,author,owner,attributions,consumerId,contentEncoding,contentDisposition,appIcon,publishCheckList,publishComment,compatibilityLevel,status,prevState,prevStatus,lastStatusChangedOn,keywords,pkgVersion,version,versionKey,language,channel,framework,subject,medium,board,gradeLevel,topic,boardIds,gradeLevelIds,subjectIds,mediumIds,topicsIds,targetFWIds,targetBoardIds,targetGradeLevelIds,targetSubjectIds,targetMediumIds,targetTopicIds,createdOn,createdFor,createdBy,lastUpdatedOn,lastUpdatedBy,lastSubmittedOn,lastSubmittedBy,publisher,lastPublishedOn,lastPublishedBy,publishError,reviewError,body,editorState,answer,solutions,instructions,hints,media,responseDeclaration,interactions,qType,scoringMode,qumlVersion,timeLimit,maxScore,showTimer,showFeedback,showSolutions,interactionTypes,templateId,bloomsLevel,feedback,responseProcessing,templateDeclaration,dailySummaryReportEnabled,allowAnonymousAccess,termsAndConditions,expectedDuration,completionCriteria,collaborators,semanticVersion,schemaVersion"
+const questionAllFields = 'name,code,description,mimeType,primaryCategory,additionalCategories,' +
+'visibility,copyright,license,lockKey,assets,audience,author,owner,attributions,consumerId,' +
+'contentEncoding,contentDisposition,appIcon,publishCheckList,publishComment,compatibilityLevel,' +
+'status,prevState,prevStatus,lastStatusChangedOn,keywords,pkgVersion,version,versionKey,language,' +
+'channel,framework,subject,medium,board,gradeLevel,topic,boardIds,gradeLevelIds,subjectIds,mediumIds,' +
+'topicsIds,targetFWIds,targetBoardIds,targetGradeLevelIds,targetSubjectIds,targetMediumIds,targetTopicIds,' +
+'createdOn,createdFor,createdBy,lastUpdatedOn,lastUpdatedBy,lastSubmittedOn,lastSubmittedBy,publisher,' +
+'lastPublishedOn,lastPublishedBy,publishError,reviewError,body,editorState,answer,solutions,instructions,' +
+'hints,media,responseDeclaration,interactions,qType,scoringMode,qumlVersion,timeLimit,maxScore,showTimer,' +
+'showFeedback,showSolutions,interactionTypes,templateId,bloomsLevel,feedback,responseProcessing,' +
+'templateDeclaration,dailySummaryReportEnabled,allowAnonymousAccess,termsAndConditions,expectedDuration,' +
+'completionCriteria,collaborators,semanticVersion,schemaVersion'
 
 /**
  * This function helps to get all domain from ekstep
@@ -19,7 +29,7 @@ const questionAllFields = "name,code,description,mimeType,primaryCategory,additi
  * @param {Object} response
  */
 
-function readQuestion(identifier, query, headers) {
+function readQuestion (identifier, query, headers) {
   return (new Promise((resolve, reject) => {
     contentProviderUtil.readQuestion(identifier, query, headers, (error, res) => {
       if (error) {
@@ -35,55 +45,47 @@ function readQuestion(identifier, query, headers) {
   }))
 }
 
-
-function getList(req, response) {
+function getList (req, response) {
   delete req.headers['accept-encoding']
-  logger.debug({ msg: 'questionService.getList() called' }, req)
+  utilsService.logDebugInfo('question.list', req.rspObj, 'question list api called')
   let data = {}
   let rspObj = req.rspObj
   data.body = req.body
   req.query.fields = req.query.fields || questionAllFields
   data.query = req.query
 
-  let questionIds = _.get(data, 'body.request.search.identifier');
+  let questionIds = _.get(data, 'body.request.search.identifier')
   if (_.isEmpty(questionIds) || !_.isArray(questionIds)) {
     rspObj.responseCode = responseCode.CLIENT_ERROR
-    rspObj.errMsg = 'Either identifier is missing or it is not list type';
-    logger.error({
-      msg: 'Either identifier is missing or it is not list type',
-      additionalInfo: { data },
-      err: { responseCode: rspObj.responseCode }
-    }, req)
+    rspObj.errMsg = 'Either identifier is missing or it is not list type'
+    utilsService.logErrorInfo('question.list', rspObj, 'Either identifier is missing or it is not list type')
     return response.status(400).send(respUtil.errorResponse(rspObj))
   }
-  const questionsLimit = parseInt(process.env.questions_list_limit, 10) || 20;
-  questionIds = _.take(questionIds, questionsLimit);
+  const questionsLimit = parseInt(process.env.questions_list_limit, 10) || 20
+  questionIds = _.take(questionIds, questionsLimit)
 
-  logger.debug({ msg: 'Request to get questions by ids ', additionalInfo: { questionIds } }, req)
+  utilsService.logDebugInfo('question.list', rspObj, 'Request to get questions by ids')
 
   const questionsRequestPromises = questionIds.map(id => {
     return readQuestion(id, req.query, req.headers)
   })
 
   Promise.all(questionsRequestPromises).then(questionResponses => {
-    rspObj.result = {questions: []};
+    rspObj.result = {questions: []}
     rspObj.result.questions = questionResponses.map(questionResponse => {
       return _.get(questionResponse, 'result.question')
-    });
-    rspObj.result.count = questionResponses.length;
-    logger.debug({ msg: 'questions details', additionalInfo: { questionResponses } }, req);
-    return response.status(200).send(respUtil.successResponse(rspObj));
+    })
+    rspObj.result.count = questionResponses.length
+    utilsService.logDebugInfo('question.list', rspObj, 'questions details')
+    return response.status(200).send(respUtil.successResponse(rspObj))
   }).catch(err => {
     rspObj.responseCode = _.get(err, 'responseCode') || responseCode.SERVER_ERROR
-    logger.error({ msg: 'Getting error  fetching questions by ids ', additionalInfo: { questionIds }, err: { err, responseCode: rspObj.responseCode } }, req)
+    utilsService.logErrorInfo('question.list', rspObj, 'Getting error  fetching questions by ids')
     var httpStatus = err && err.statusCode >= 100 && err.statusCode < 600 ? err.statusCode : 500
     rspObj.result = err && err.result ? err.result : {}
     rspObj = utilsService.getErrorResponse(rspObj, err)
     return response.status(httpStatus).send(respUtil.errorResponse(rspObj))
-  });
+  })
 }
 
-
-
 module.exports.getList = getList
-
