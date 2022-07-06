5701290416e486a914b18988a4e9e64315bfc8b0
Harish Kumar Gangula <harish@sanketika.in>
2022-07-06 14:08:19 +0530

    Issue SB-29893 fix: sonar analysis

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ org=$3
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
+
