683c6834441470725b5a45c2216f5dedbbbbfeb4
vayu068 <vayu068@gmail.com>
2022-08-07 23:29:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ name=knowledge-mw-service
 node=$2
 org=$3
 
-[[ -f ImageMagick-i386-pc-solaris2.11.tar.gz ]] || wget https://www.imagemagick.org/download/binaries/ImageMagick-i386-pc-solaris2.11.tar.gz
+[[ -f ImageMagick-i386-pc-solaris2.11.tar.gz ]] || wget https://imagemagick.org/archive/binaries/ImageMagick-i386-pc-solaris2.11.tar.gz
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
